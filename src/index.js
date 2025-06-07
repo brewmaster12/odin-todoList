3c453c96790f8d7bdbd4902f5b81501b839a2fa7
brewmaster12 <seanywg@gmail.com>
2025-06-07 14:11:24 +0100

    Create bringUpTodoForm function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {makeProject} from "./project.js";
-
+/* import { bringUpTodoForm } from "./todoForm.js";
+ */
 const projectsList = document.getElementById("projectsList");
 
 /* New project button, brings up form to enter project name */
@@ -17,21 +18,18 @@ submitProjectBtn.addEventListener("click", (event) => {
     projectDialog.close();
     todosList.textContent = "";
     submitProject(projectNameField.value);
+    /* const project = makeProject(projectNameField.value) */
 });
 
 /* Todo form info */
-const todoName = document.getElementById("todoName");
+/* const todoName = document.getElementById("todoName");
 const description = document.getElementById("description");
 const dueDate = document.getElementById("dueDate");
-const priority = document.getElementById("priority");
-
-const todoDialog = document.getElementById("todoDialog");
+const priority = document.getElementById("priority"); */
 
 const heading = document.getElementById("heading");
-
 const newTodoDiv = document.getElementById("newTodoDiv");
-
-const todosList = document.getElementById("todosList")
+const todosList = document.getElementById("todosList");
 
 
 /* Project function */
@@ -57,8 +55,7 @@ function submitProject(projectName) {
     const newTodoBtn = document.createElement("button");
     newTodoBtn.textContent = "New todo";
     newTodoBtn.addEventListener("click", () => {
-        /* Brings up form to enter todo info */
-        todoDialog.showModal();
+        bringUpTodoForm();
     });
 
     /* Append elements to DOM */
@@ -69,13 +66,73 @@ function submitProject(projectName) {
     newTodoDiv.appendChild(newTodoBtn);
 
 
-    /* Submit todo button */
-    const submitTodoBtn = document.getElementById("submitTodoBtn");
-    submitTodoBtn.addEventListener("click", (event) => {
-        event.preventDefault();
-        todoDialog.close();
-        submitTodo(todoName.value, description.value, dueDate.value, priority.value);
-    })
+    function bringUpTodoForm() {
+        // Create dialog
+        const todoDialog = document.createElement("dialog");
+        todoDialog.id = "todoDialog";
+
+        // Create form
+        const todoForm = document.createElement("form")
+        todoForm.id = "todoForm"
+
+        // Create form elements
+        const todoNameLabel = document.createElement('label');
+        todoNameLabel.textContent = 'Enter todo:';
+        const todoName = document.createElement('input');
+        todoName.type = 'text';
+        todoName.id = 'todoName';
+
+        const descriptionLabel = document.createElement('label');
+        descriptionLabel.textContent = 'Description:';
+        const description = document.createElement('input');
+        description.type = 'text';
+        description.id = 'description';
+
+        const dueDateLabel = document.createElement('label');
+        dueDateLabel.textContent = 'Due date:';
+        const dueDate = document.createElement('input');
+        dueDate.type = 'text';
+        dueDate.id = 'dueDate';
+
+        const priorityLabel = document.createElement('label');
+        priorityLabel.textContent = 'Priority:';
+        const priority = document.createElement('input');
+        priority.type = 'text';
+        priority.id = 'priority';
+
+        // Submit button
+        const submitTodoBtn = document.createElement("button");
+        submitTodoBtn.textContent = "Submit";
+        submitTodoBtn.type = "submit";
+        submitTodoBtn.id = "submitTodoBtn";
+
+        // Assemble form
+        todoForm.appendChild(todoNameLabel);
+        todoForm.appendChild(todoName);
+        todoForm.appendChild(descriptionLabel);
+        todoForm.appendChild(description);
+        todoForm.appendChild(dueDateLabel);
+        todoForm.appendChild(dueDate);
+        todoForm.appendChild(priorityLabel);
+        todoForm.appendChild(priority);
+        todoForm.appendChild(submitTodoBtn);
+
+        // Append form to dialog
+        todoDialog.appendChild(todoForm);
+
+        // Add dialog to body
+        document.body.appendChild(todoDialog);
+
+        // Show the dialog
+        todoDialog.showModal();
+
+        // Form submission
+        submitTodoBtn.addEventListener("click", (event) => {
+            event.preventDefault();
+            todoDialog.close();
+            submitTodo(todoName.value, description.value, dueDate.value, priority.value);
+        });
+    }
 
     /* Create and print todo */
     function submitTodo(title, description, dueDate, priority) {
@@ -98,11 +155,15 @@ function submitProject(projectName) {
     }
 };
 
-
-
 submitProject("Default project");
 
 
+/*
+    NOTES 3/6 17:25
+    - Make new git branch
+    - Use this branch to re-write the code; do without submitProject or at least make it less unwieldy
+    - See comment in the submitProjectBtn
+*/
 
 
 /*
